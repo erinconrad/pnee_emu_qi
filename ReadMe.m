31436f272ab3b048201188bbe886a38c2ccde662
Erin <erincconrad@gmail.com>
2024-07-09 09:26:13 -0400

    numbers for fig 1

--- ReadMe.m
+++ ReadMe.m
@@ -5,7 +5,7 @@ To run this code, do the following:
 1) Download or clone the codebase: https://github.com/erinconrad/pnee_emu_qi
 2) Download myFisher23 https://www.mathworks.com/matlabcentral/fileexchange/15399-myfisher23
 and add it to your path (this is needed to do a Fisher exact test for
-gender, which is a 2x3 table)
+gender, which is non-binary and so requires a 2x3 table)
 3) Create a Matlab script file called pnee_locations.m and put it in your
 path. This should point to the data folder and the results folder. Here is
 an example of how this should look:
@@ -23,9 +23,16 @@ link and add them to your data folder:
 
 https://upenn.box.com/s/h28wvjlj4381z3ddgwhkp0avk8iepcsn
 
+Note for Erin: this data is from the redcap report called
+erin_this_is_the_one_for_the_paper in PNEE EMU Pathway redcap
+
 5) Navigate to the script folder and type
     >> main
 
 This will run the analyses and output tables and the figure to the results folder.
+
+
+Kelly Boylan and Erin Conrad
+July 2024
 
 %}
